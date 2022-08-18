32f609c8ac3b67307843534a8aa20d2fa95fd849
ClaudomiroSales <claudomiro.sales@gmail.com>
2022-08-18 10:35:11 -0300

    Teste de set e get

--- main.cpp
+++ main.cpp
@@ -6,11 +6,18 @@ using std::cout;
 
 int main( )
 {
-    cout << "Usando Sword no main\n";
+    int durability = 10;
     
-
     Sword royalGuardSword;
     royalGuardSword.print();
+
+    cout << "Usando metodo get para obter a durabilidade: " 
+         << royalGuardSword.getDurability() << "\n";
+    
+    cout << "Usando metodo set para configurar a durabilidade\n";
+    royalGuardSword.setDurability( durability );
+
+    cout << "Apos o novo valor: " << royalGuardSword.getDurability() << "\n";
 
     return 0;
 
